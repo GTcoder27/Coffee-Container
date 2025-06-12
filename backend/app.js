01bda7b327bcd142b884d9159274ce7dc3527053
GTcoder27 <girishnandanwar2708@gmail.com>
2025-06-12 15:48:12 +0530

    ready to deploy

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const os = require('os');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 // CORS configuration
 const corsOptions = {
